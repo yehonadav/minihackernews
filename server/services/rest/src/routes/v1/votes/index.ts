84c7ce9f03246e76712e078a8288c7c577dc6676
yonadav <yonadav.barilan@gmail.com>
2022-04-13 10:52:24 +0300

    make transaction faster

--- server/services/rest/src/routes/v1/votes/index.ts
+++ server/services/rest/src/routes/v1/votes/index.ts
@@ -49,16 +49,19 @@ const votes: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
 
       // when dealing in high load it would
       // make more sense to queue this transaction
-      await db.conn.transaction(async (session) => {
-        // votes are indexed to be unique by userId & postId
-        // so that ensures vote does not exist (exception is thrown otherwise)
-        // TODO: handle already exist exception
-        await db.votes.create({userId: objectId(userId), postId: objectId(postId)}, { session });
+      await db.conn.transaction(async (session) =>
+        // TODO: this will improve the transaction speed,
+        //  but a better approach would be to use a bulk
+        Promise.all([
 
-        // we need a way to ensure this happens if a vote is created,
-        // a transaction might be a good way to go about this
-        await db.posts.findOneAndUpdate({_id: objectId(postId)}, {$inc: {votes:1}}, { session });
-      });
+          // votes are indexed to be unique by userId & postId
+          // so that ensures vote does not exist (exception is thrown otherwise)
+          db.votes.create({userId: objectId(userId), postId: objectId(postId)}, { session }),
+
+          // we need a way to ensure this happens if a vote is created,
+          // a transaction might be a good way to go about this
+          db.posts.findOneAndUpdate({_id: objectId(postId)}, {$inc: {votes:1}}, { session }),
+      ]));
 
       reply.send('');
     }
